1cdd2e0a570812c5d2983fef58bb3efea9e1a84f
Leonel Alejandro Ceballos Alvarado <lceballos5@ucol.mx>
2024-11-06 20:54:45 -0600

    set up

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
+import { Routes, Route } from "react-router-dom"
 import Input from "./components/Input"
+import SingUpPage from "./pages/SingUpPage"
+import LoginPage from "./pages/LoginPage"
 
 function App() {
   return (
-    <>
-    <Input label={"Usuario"}/>
-    <h1 className='text-red-500'>Hello</h1>
-    </>
+    <div>
+      <Input label={"Usuario"}/>
+      <h1 className='text-red-500'>Hello</h1>
+      <Routes>
+        <Route path="/" element={"home"}/>
+        <Route path="signup" element={<SingUpPage/>}/>
+        <Route path="login" element={<LoginPage/>}/>
+      </Routes>
+    </div>
   )
 }
 
